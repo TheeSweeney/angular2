2b22b6764e820d5eb5a84609547a6d048fc203f8
TheeSweeney <bsweeney930@gmail.com>
2018-03-12 11:28:35 -0400

    test of typescipt;

--- newStart/nontyped.js
+++ newStart/nontyped.js
@@ -1,7 +1,7 @@
-function multiply(x, y) {
-    return x * y;
-}
+// function multiply(x, y) {
+//     return x * y;
+// }
 
-console.log(multiply (2, 3));
+// console.log(multiply (2, 3));
 
-console.log(multiply ('bob', 3))
+// console.log(multiply ('bob', 3))
--- newStart/typed.ts
+++ newStart/typed.ts
@@ -1,1 +1,8 @@
 
+function multiply (x: number, y: number) {
+    return x * y;
+}
+
+multiply(2, 3)
+
+multiply(2, "test")
