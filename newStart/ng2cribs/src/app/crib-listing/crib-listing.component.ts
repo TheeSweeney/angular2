dbb3b856623ef3a067fc88d441ce8f9f4bfe6448
TheeSweeney <bsweeney930@gmail.com>
2018-04-18 14:46:36 -0400

    added cribs data;

--- newStart/ng2cribs/src/app/crib-listing/crib-listing.component.ts
+++ newStart/ng2cribs/src/app/crib-listing/crib-listing.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crib-listing.component.html',
   styleUrls: ['./crib-listing.component.css']
 })
-export class CribListingComponent implements OnInit {
+export class CribListingComponent implements OnInit {//OnInit is a lifecycle hook that allows us to run something whenevr the component starts up/gets initialized
 
   constructor() { }
 
