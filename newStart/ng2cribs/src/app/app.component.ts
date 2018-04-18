681d0625ad21488b21b68475af7db30191c9d873
TheeSweeney <bsweeney930@gmail.com>
2018-04-18 14:39:40 -0400

    added notes to describe various parts of main component and how they work/fit together; created criblisting component (blank);

--- newStart/ng2cribs/src/app/app.component.ts
+++ newStart/ng2cribs/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',//custom html element that will allow us to insert the root component into our index.html
+  templateUrl: './app.component.html',//views that will be used by component; we could define the view right here or give a path to another file
+  styleUrls: ['./app.component.css']//similarly, style could be added here, but again it is in a separate file - SOC yo
 })
 export class AppComponent {
-  title = 'app';
+  title = 'butts';
 }
