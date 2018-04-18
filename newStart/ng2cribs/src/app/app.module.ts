76dcb8c399e4c02fa5b58a20f565b4a2f250de3c
TheeSweeney <bsweeney930@gmail.com>
2018-04-18 14:39:40 -0400

    added notes to describe various parts of main component and how they work/fit together; created criblisting component (blank);

--- newStart/ng2cribs/src/app/app.module.ts
+++ newStart/ng2cribs/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
+    //any time a new componenet is added it will also be placed in this list
+    //if we add it through the cli using the command `ng g component [component-name]` it will automatically add it here and create a component file with a boilerplate test suite, css, html, and ts file 
   ],
   imports: [
     BrowserModule
+    //this list modules that will be imported into the app
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//any injectable services will be added here
+  bootstrap: [AppComponent]//which component is the top level component?
 })
 export class AppModule { }
