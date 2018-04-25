056b7d12cc99ecf57f2d940c3399365a7d35be9b
TheeSweeney <bsweeney930@gmail.com>
2018-04-25 14:22:21 -0400

    created crib interface; imported Crib into crib.ts; set crib to type Crib in crib component;

--- newStart/ng2cribs/src/app/crib-card/crib-card.component.ts
+++ newStart/ng2cribs/src/app/crib-card/crib-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Crib } from './../crib';
 @Component({
   selector: 'app-crib-card',
   templateUrl: './crib-card.component.html',
@@ -7,12 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CribCardComponent implements OnInit {
 
-  @Input('crib') crib: any;//we want to accept binding called crib and assign it to a local property called crib
+  @Input('crib') crib: Crib;//we want to accept binding called crib and assign it to a local property called crib
   //anything passed into crib property is locally referred to as crib and we can use it in our view
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
